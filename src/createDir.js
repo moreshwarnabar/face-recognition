dd130a4044b5813766ff23b6c087f9adfd993827
moreshwarnabar <mrnabar@gmail.com>
2024-03-22 07:41:54 -0700

    added code to create a directory to save frames

---
+++ src/createDir.js
@@ -0,0 +1,11 @@
+import fs from 'node:fs';
+
+export function createDirectory(dirName) {
+  try {
+    if (!fs.existsSync(`tmp/${dirName}`)) {
+      fs.mkdirSync(`tmp/${dirName}`);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
--- src/splitter.js
+++ src/splitter.js
@@ -1,19 +1,21 @@
 import ffmpeg from 'fluent-ffmpeg';
-import path from 'path';
+import { createDirectory } from './createDir.js';
 
 export async function videoSplitter(video) {
-  // const pathToVideo = path.resolve(video);
+  const dirName = video.split('.')[0].split('/')[1];
+  createDirectory(dirName);
+  console.log(dirName);
 
-  await new Promise((resolve, reject) => {
-    ffmpeg()
-      .input(video)
-      .inputFPS(1)
-      .seekInput(0)
-      .frames(10)
-      .videoFilters('fps=1/10')
-      .save('tmp/output-%02d.jpg')
-      .on('start', commandLine => console.log('Command: ' + commandLine))
-      .on('end', () => resolve())
-      .on('error', error => reject(new Error(error)));
-  });
+  // await new Promise((resolve, reject) => {
+  //   ffmpeg()
+  //     .input(video)
+  //     .inputFPS(1)
+  //     .seekInput(0)
+  //     .frames(10)
+  //     .videoFilters('fps=1/10')
+  //     .save('tmp/output-%02d.jpg')
+  //     .on('start', commandLine => console.log('Command: ' + commandLine))
+  //     .on('end', () => resolve())
+  //     .on('error', error => reject(new Error(error)));
+  // });
 }
