1d3b2eff11f603731bf6f62e0b904a0983ab8176
moreshwarnabar <mrnabar@gmail.com>
2024-03-25 11:52:57 -0700

    added code to download video from s3

--- index.ts
+++ index.ts
@@ -1,17 +1,13 @@
 import { S3Event } from 'aws-lambda';
-import {
-  HeadBucketCommandOutput,
-  HeadObjectCommand,
-  S3Client,
-} from '@aws-sdk/client-s3';
-import { videoSplitter } from './src/splitter';
+import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import fs from 'fs';
+import { createDirectory } from './src/createDir';
+import internal from 'stream';
 
 const s3 = new S3Client({ region: 'us-east-1' });
 
-export const handler = async (
-  event: S3Event
-): Promise<HeadBucketCommandOutput | undefined> => {
-  console.log(`EVENT: ${event}`);
+export const handler = async (event: S3Event): Promise<String | undefined> => {
+  // extract the required parameters from the event
   const bucket = event.Records[0].s3.bucket.name;
   const key = decodeURIComponent(
     event.Records[0].s3.object.key.replace(/\+/g, '')
@@ -20,13 +16,22 @@ export const handler = async (
     Bucket: bucket,
     Key: key,
   };
-  console.log(`PARAMS: ${params}`);
+  console.log(`KEY: ${params.Key}`);
+
+  createDirectory('videos');
 
   try {
     // fetch the video from the bucket
-    const response = await s3.send(new HeadObjectCommand(params));
-    console.log(`RESPONSE: ${response}`);
-    return response;
+    const response = await s3.send(new GetObjectCommand(params));
+    const fileStream = fs.createWriteStream('videos/');
+    if (response.Body instanceof internal.Readable) {
+      response.Body.pipe(fileStream);
+    } else {
+      console.log(
+        `GetObjectCommand should return an internal.Readable object. Maybe the code is running in the Browser?`
+      );
+    }
+    return 'Completed successfully!';
   } catch (err) {
     console.log(err);
     const message = `Error fetching video ${key} from bucket ${bucket}`;
