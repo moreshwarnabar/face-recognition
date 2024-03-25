8dee220b931f58b7835f0f3298f245373d13d9a6
moreshwarnabar <mrnabar@gmail.com>
2024-03-25 14:51:59 -0700

    tested code for downloading video from s3

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 import { S3Event } from 'aws-lambda';
 import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
-import fs from 'fs';
-import { createDirectory } from './src/createDir';
-import internal from 'stream';
+import fs from 'node:fs';
+import { writeFile } from 'node:fs/promises';
+import { Readable } from 'node:stream';
 
-const s3 = new S3Client({ region: 'us-east-1' });
+const s3 = new S3Client({ region: 'us-east-1', logger: undefined });
 
 export const handler = async (event: S3Event): Promise<String | undefined> => {
   // extract the required parameters from the event
@@ -18,20 +18,21 @@ export const handler = async (event: S3Event): Promise<String | undefined> => {
   };
   console.log(`KEY: ${params.Key}`);
 
-  createDirectory('videos');
-
   try {
     // fetch the video from the bucket
     const response = await s3.send(new GetObjectCommand(params));
-    const fileStream = fs.createWriteStream('videos/');
-    if (response.Body instanceof internal.Readable) {
-      response.Body.pipe(fileStream);
-    } else {
-      console.log(
-        `GetObjectCommand should return an internal.Readable object. Maybe the code is running in the Browser?`
-      );
+    console.log('Writing video to file');
+    const body = await response.Body?.transformToString();
+    if (body != undefined) {
+      console.log('Body is present');
+      await writeFile(`/tmp/${key}`, body);
     }
-    return 'Completed successfully!';
+
+    if (fs.existsSync(`/tmp/${key}`)) {
+      console.log('File exists');
+      return 'Completed successfully!';
+    }
+    return 'Completed unsuccessfully!';
   } catch (err) {
     console.log(err);
     const message = `Error fetching video ${key} from bucket ${bucket}`;
