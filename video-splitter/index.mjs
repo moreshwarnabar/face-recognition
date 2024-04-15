2a408434b3d6074b00cff14805b9191c4897c8fc
moreshwarnabar <mrnabar@gmail.com>
2024-04-15 11:36:26 -0700

    updated build process

--- video-splitter/index.mjs
+++ video-splitter/index.mjs
@@ -1,24 +1,24 @@
-import * as esbuild from 'esbuild';
-import path from 'path';
+import * as esbuild from "esbuild";
+import path from "path";
 
 let resolveFfmpegPlugin = {
-  name: 'resolveFfmpeg',
+  name: "resolveFfmpeg",
   setup(build) {
-    build.onResolve({ filter: /lib-cov\/fluent-ffmpeg/ }, args => {
+    build.onResolve({ filter: /lib-cov\/fluent-ffmpeg/ }, (args) => {
       // fix https://github.com/fluent-ffmpeg/node-fluent-ffmpeg/issues/573
-      const actualPath = path.join(args.resolveDir, 'lib', 'fluent-ffmpeg.js');
+      const actualPath = path.join(args.resolveDir, "lib", "fluent-ffmpeg.js");
       return { path: actualPath };
     });
   },
 };
 
 await esbuild.build({
-  entryPoints: ['src/app.ts'],
+  entryPoints: ["src/app.ts"],
   bundle: true,
   minify: true,
   sourcemap: true,
-  platform: 'node',
-  target: 'es2020',
-  outfile: 'index.js',
+  platform: "node",
+  target: "es2020",
+  outfile: "dist/index.js",
   plugins: [resolveFfmpegPlugin],
 });
