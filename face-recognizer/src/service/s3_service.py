bca9070ab7a722d02713d4800f083d4522f56cff
moreshwarnabar <mrnabar@gmail.com>
2024-04-16 18:39:32 -0700

    resolved failures when testing in lambda

--- face-recognizer/src/service/s3_service.py
+++ face-recognizer/src/service/s3_service.py
@@ -1,18 +1,23 @@
 import boto3
 import botocore
+import os
 
 s3 = boto3.resource('s3')
 
-def download_frame(bucket, key):
+def download_file(bucket, key):
+    file_path = f'/tmp/{key}'
     try:
-        s3.Bucket(bucket).download_file(key, f'/tmp/{key}')
+        s3.Bucket(bucket).download_file(key, file_path)
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == "404":
-            print("The frame does not exist.")
+            print("The file does not exist.")
+            print(e.response['Error'])
         else:
             raise
+    return file_path
 
 
 def upload_result(file_name):
     bucket = '1229975385-output'
-    s3.upload_file(file_name, bucket, file_name)
+    key = os.path.basename(file_name)
+    s3.Bucket(bucket).upload_file(file_name, key)
